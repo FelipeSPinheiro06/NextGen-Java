064e7cf0ec78096f815d77ef2ca3105bc2a23215
FelipePinheiro <fsp12371@git.com>
2024-04-01 11:36:53 -0300

    first commit

---
+++ src/test/java/com/fiap/nextgen/NextgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.nextgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
