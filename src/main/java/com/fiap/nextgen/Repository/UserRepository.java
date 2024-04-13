0b8bf7f46e50b34a08410604d444f53c3cfd01b3
FelipePinheiro <fsp12371@git.com>
2024-04-13 12:40:20 -0300

    Persistência de Dados Aplicada

---
+++ src/main/java/com/fiap/nextgen/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fiap.nextgen.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fiap.nextgen.Model.Users;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    
+}
