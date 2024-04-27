0d2576406eacea0cbcbc315e0d64be05e591a160
FelipePinheiro <fsp12371@git.com>
2024-04-27 11:58:08 -0300

    Métodos parcialmente adicionados

--- src/main/java/com/fiap/nextgen/Repository/FeedbackRepository.java
+++ src/main/java/com/fiap/nextgen/Repository/FeedbackRepository.java
@@ -5,5 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fiap.nextgen.Model.Feedback;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
+    // @Query("SELECT f FROM Feedback f WHERE MONTH(f.date) = ?2 AND f.company.name = ?1 ")
+    // List<Feedback> findByCompanyNameAndMes(String company, Integer mes, Pageable pageable);
+
+    
+    // List<Feedback> findByMes(Integer mes, Pageable pageable);
+
+    // List<Feedback> findByCompany(String company, Pageable pageable);
     
 }
